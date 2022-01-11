2154a314a3bffdc9d9c3054a14e6041ef684ed13
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-01-11 09:48:59 +0400

    apply useState react hook in counter component to make count dynamic on page

--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,21 @@
+import React, { useState } from "react";
+// React Hooks are prefixed with 'use'
+// useState is a React Hook
+// State is data that live in component, data updates (dynamic) and allows component to be interactive
+// State could be data from a DB, or accordion/ modal is open or not
+
 export function Counter({  }) {
+
+  // value of state and updater of state
+  // set default state of count to zero
+  //  const [ state, setState ] = useState(defaultState);
+  const [ count, setCount ] = useState(0);
+
   return (
       <div>
-        <h3>0</h3>
-        <button>+</button>
+        <h3>{count}</h3>
+        <button onClick={() => setCount(count - 1)} >-</button>
+        <button onClick={() => setCount(count + 1)} >+</button>
       </div>
   );
 }

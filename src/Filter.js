993513465511a635ea6dbf8e2cd40ed78339205f
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-01-12 09:08:09 +0400

    add comments on state for parent chile component relationship in Filter component

--- src/Filter.js
+++ src/Filter.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 // lifting state from MovieList by passing the state and setState as props
+// MovieList is the parent, and Parent needs to own the state
+// State flows downwards from Parent to Child in component tree
+// useContext Hook is the alternative, to manipulate state from anywhere in component tree
 export function Filter({ filter, setFilter }) {
   return (
     <label>

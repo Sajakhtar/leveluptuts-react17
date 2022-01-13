fdf56f1fd4d3a4ec99d6f5acf73b4f5d5b6b47ac
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2022-01-13 17:59:01 +0400

    change poster size

--- src/movies/Movie.js
+++ src/movies/Movie.js
@@ -11,7 +11,7 @@ export function Movie({ movie, config }) {
   return (
     <li>
       { config.images?.base_url && (
-        <img src={config.images.base_url + config.images.poster_sizes[1] + movie.poster_path} alt={movie.title + " Poster"} />
+        <img src={config.images.base_url + config.images.poster_sizes[2] + movie.poster_path} alt={movie.title + " Poster"} />
       )}
       <h3>{movie.title}</h3>
     </li>
